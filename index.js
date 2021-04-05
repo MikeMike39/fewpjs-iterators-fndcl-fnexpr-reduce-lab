f6d5cb41e435e85c452d2a18468737ef0aa0ae77
MikeMike39 <Mfonseca294@gmail.com>
2021-04-04 21:01:54 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Creates "totalBatteries variable"; should have a number as a result; should have made the sum of all the assembled batteries
+const totalBatteries = batteryBatches.reduce(function(total, batches){
+    return batches + total
+    }
+  )
